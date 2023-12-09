8ed5517ca545513dce206324b40f115e096f1ac5
espenraugstad <espen.raugstad@gmail.com>
2023-12-09 01:09:00 +0100

    Added an interactive island with Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://playful-tapioca-f4ebd1.netlify.app/"
+  site: "https://playful-tapioca-f4ebd1.netlify.app/",
+  integrations: [preact()]
 });
